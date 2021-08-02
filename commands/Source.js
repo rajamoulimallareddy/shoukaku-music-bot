6c9bca2181ff5352da1dfe98866e7319508ab97c
rajamoulimallareddy <68652418+rajamoulimallareddy@users.noreply.github.com>
2021-08-02 09:50:56 +0530

    Update Source.js

--- commands/Source.js
+++ commands/Source.js
@@ -8,7 +8,6 @@ module.exports = {
     guildOnly: false,
     args: false,
     usage: '',
-    ownerOnly: true,
     execute: async (message, args, client) => {
         message.channel.send(client.util.embed().setDescription('❱・[Source Code](https://github.com/rajamoulimallareddy/shoukaku-music-bot)\n❱・[Support Server](https://discord.gg/dB6RzCbZhW)'));
     }
