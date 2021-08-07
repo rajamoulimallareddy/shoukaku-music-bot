a4fc7b1389c7504cf64cc546f66b6c2b55998e1a
rajamoulimallareddy <68652418+rajamoulimallareddy@users.noreply.github.com>
2021-08-07 19:10:18 +0530

    forgot something

--- commands/Skip.js
+++ commands/Skip.js
@@ -6,18 +6,15 @@ module.exports = {
     guildOnly: false,
     args: false,
     usage: '[number]',
-    inVC: true,
-    sameVc: true,
-    playing: true,
     execute: async (message, args, client) => {
         const skipTo = args[0] ? parseInt(args[0], 10) : null;
-        /*if (!message.member.voice.channelId)
+        if (!message.member.voice.channelId)
             return await message.reply({ embeds: [client.util.embed().setDescription('You are not in a voice channel to perform this.').setColor('RED')], allowedMentions: { repliedUser: false } });
+        const MusicDispatcher = client.queue.get(message.guild.id);
         if (!MusicDispatcher || !MusicDispatcher.current)
             return await message.reply({ embeds: [client.util.embed().setDescription('There is Nothing playing in thie guild.').setColor('RED')], allowedMentions: { repliedUser: false } });
         if (MusicDispatcher.player.connection.channelId !== message.member.voice.channelId)
-            return await message.reply({ embeds: [client.util.embed().setDescription('You are not in the same voice channel where I am.').setColor('RED')], allowedMentions: { repliedUser: false } });*/
-        const MusicDispatcher = client.queue.get(message.guild.id);
+            return await message.reply({ embeds: [client.util.embed().setDescription('You are not in the same voice channel where I am.').setColor('RED')], allowedMentions: { repliedUser: false } });
 
         if (skipTo !== null && (isNaN(skipTo) || skipTo < 1 || skipTo > MusicDispatcher.queue.length)) return message.reply({ embeds: [client.util.embed().setDescription('Number Is Invalid Or Exceeds Queue Length.')], allowedMentions: { repliedUser: false } });
         try {
